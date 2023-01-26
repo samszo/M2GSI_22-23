c84b9e170127d80fef6841be316bcd87bf14fc4c
Samuel Szoniecky <samszon@gmail.com>
2023-01-26 13:37:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var projets = ['EURUA','Ferielle_Bachir','Hyphe_Messali_Felix','Imane-Rezika-analyse'
     ,'JupiteretChafea','LIASD','Mariette_et_Helenya','Projet_Ariane_Aissatou'];
-var fichiers = ['.md','.json','.gexf','-2.gexf','-3.gexf'];
+var fichiers = ['.html','.md','.json','.gexf','-2.gexf','-3.gexf'];
 
 /*
 for (let index = 0; index < projets.length; index++) {
@@ -13,10 +13,15 @@ var titreProjet = d3.select('body').select('h2').data(projets).enter().append('d
 titreProjet.append('h2').html(p=>p);
 titreProjet.append('div').html(p=>{
     let html = '';
-    fichiers.forEach(f=>{
+    fichiers.forEach(f=>{        
         let pathSource = "data/"+p+f, 
-            pathWeb = "https://samszo.github.io/M2GSI_22-23/data/"+p+f;
-        html += `<a target="_blank" href="${pathSource}">${pathSource}</a> <a target="_blank" href="${pathWeb}">${pathWeb}</a> <br/>`;
+            pathWeb = "https://samszo.github.io/M2GSI_22-23/data/"+p+f,
+            pathHTML = "https://samszo.github.io/M2GSI_22-23/pages/"+p+f;
+        if(f=='.html'){
+            html += `<a target="_blank" href="${pathHTML}">${pathHTML}</a><br/><br/>`;
+        }else{
+            html += `<a target="_blank" href="${pathSource}">${pathSource}</a> <a target="_blank" href="${pathWeb}">${pathWeb}</a> <br/>`;
+        }
     })
     return html;
 })
